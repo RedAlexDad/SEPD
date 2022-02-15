ed55f050917ab4fecc0e0c97d11714898846b209
RedAlexDad <boss6852@gmail.com>
2022-02-15 22:03:41 +0300

    made the server working

--- app.js
+++ app.js
@@ -11,5 +11,8 @@ app.get('*', (req, res) => { res.sendFile(path.resolve(__dirname, 'client', 'ind
 // Номер порта
 const PORT = 5000;
 
+// Введите npm run dev
+// Чтобы запустить сервер
+
 // Печать на консольное окно
 app.listen(PORT, () => console.log(`Server has been started on port ${PORT}...`));
