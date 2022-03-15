585370288c6666556c9f278336f83e55d5095fe1
Ekaterina2022 <katerina_kharitonova@mail.ru>
2022-03-15 23:07:52 +0300

    edit web main

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -2,8 +2,52 @@ import React from 'react';
 
 export const Main = () => {
     return (
-      <div>
-        <h1> MAIN WEB </h1>
-      </div>
+        <div> 
+        
+         <title>Авторизация</title> 
+        
+         
+        <body> 
+         <header> 
+         <div class="LOGO2"> 
+         {/* <img src="image/2.png">  */}
+         </div> 
+         </header> 
+         <center> 
+         
+         <h1> 
+         Авторизация 
+         </h1> 
+         <div class="parent"> 
+         <form> 
+         <div class="form-group"> 
+         <label for="exampleInputEmail1">Телефон</label> 
+         {/* <input type="phone" class="form-control" id="exampleInputP  */}
+         {/* hone1" placeholder="+7 (XXX) XXX XXXX">  */}
+         </div> 
+         <div class="form-group"> 
+         <label for="exampleInputPassword1">Пароль</label> 
+         {/* <input type="password" class="form-control" id="exampleInputPassword1" placeholder="**">  */}
+         </div> 
+         <div class="form-check"> 
+         {/* <input type="checkbox" class="form-check-input" id="exampleCheck1">  */}
+         <label class="form-check-label" for="exampleCheck1">Запомнить</label> 
+         </div> 
+         <button type="submit" class="btn btn-outline-warning">Войти</button> 
+         <div class="form-group"> 
+         <button type="submit" class="btn btn-outline-dark" href="http://127.0.0.1:5500/SEPD/index0/index.html">Регистрация</button> 
+         
+         </div> 
+         </form> 
+         </div> 
+         
+         </center> 
+         
+         <main> 
+         
+         </main> 
+           
+        </body> 
+        </div>
     )
 }
