c64de75bc3574c22418d1ea29237789eb6728d34
RedAlexDad <boss6852@gmail.com>
2022-03-18 13:53:37 +0300

    add switch for link web-site

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -7,24 +7,14 @@ import teacerandtranslater from '../image/teacerandtranslater.jpeg';
 import translater from '../image/translater.jpg';
 // Подключаем css для визуала
 import '../css/styles.css';
+// Для переключения других веб страниц
+import { Link } from "react-router-dom";
 
 export const Main = () => {
     return (
         <div>
 
             <title>Главная</title>
-            {/* <meta charset="UTF-8"> */}
-                {/* <link rel="stylesheet" href="styles.css"> */}
-                {/* <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous"> */}
-                {/* <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"> */}
-                {/* <link rel="preconnect" href="https://fonts.googleapis.com"> */}
-                {/* <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> */}
-                {/* <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@700&display=swap" rel="stylesheet">   */}
-                <style>
-                    body
-                    {/* {  font - family: 'Montserrat', sans-serif;  } */}
-                </style>
-
                     <header>
                         <div class="LOGO2">
                             <img src={logotip} alt={"logotip"}/> 
@@ -33,10 +23,14 @@ export const Main = () => {
                     <center>
                         <div class="menu">
                             <ul>
-                                <li><a class="color-menu" href="http://127.0.0.1:5500/SEPD/index1/index.html">Главная</a></li>
-                                <li><a class="color-menu" href="D:\Users\Екатерина Харитонова\Pictures\GitHub\SEPD\index0\index.html">Запись</a></li>
-                                <li><a class="color-menu" href="http://127.0.0.1:5500/SEPD/index3/index.html">Контакты</a></li>
-                                <li><a class="color-menu" href="http://127.0.0.1:5500/SEPD/index4/index.html">Войти</a></li>
+                                <li><a class="color-menu" href>
+                                <Link to="/main">Главная</Link></a></li>
+                                <li><a class="color-menu" href>
+                                <Link to="/contact_personal">Запись</Link></a></li>
+                                <li><a class="color-menu" href>
+                                <Link to="/contact_personal">Контакты</Link></a></li>
+                                <li><a class="color-menu" href>
+                                <Link to="/contact_personal">Войти</Link></a></li>
                             </ul>
                         </div>
                         <div class="position-relative photo-text">
