1e87d027414f11b088651e6f76d9297af317c3f1
RedAlexDad <boss6852@gmail.com>
2022-03-19 19:01:06 +0300

    add

--- client/src/pages/RequestTranslatorTasks.js
+++ client/src/pages/RequestTranslatorTasks.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
-import { useHttp } from '../hooks/http.hook';
-// import user from '../../../models/user.js';
+import React, { useEffect, useState } from 'react';
+import { useHttp } from "../hooks/http.hook";
+import axios from 'axios';
 
 // Подключаем картинку
 import logotip from "../image/logotip.png";
@@ -14,46 +14,50 @@ import "../css/styles.css";
 // Для переключения других веб страниц
 import { Link } from "react-router-dom";
 
-
 // Получение данные оттуда
 // import user from '../../../models/user';
 
-
 export const RequestTranslatorTasks = () => {
-  const {loading, request} = useHttp()
+  const { loading, request } = useHttp();
+
+  const [post, setPost] = useState([])
+
+  useEffect(() => {
+    const res = axios.get('http://localhost:3000/request')
+      .then(response => {
+        setPost(response.data)
+        console.log(response.data)
+      });
+    return res.data
+  }, [])
+
   // Работа с запросами
   const [form, setForm] = useState({
-      // Содержание структуры
-      building: '', auditorium: '', discipline: '', schedule: '',
-  })
-
-  // Обработка данные
-  const hander_building = event => {
-    setForm({ ...form, [event.target.name]: event.target.value })
-}
-  const registerHandler = async () => {
-    try {
-        const data = await request('/api/request', 'POST', {...form});
-        // const data = await response('/api/request', 'POST', {...form});
-        // console.log('Data: ', data);
-    }
-    catch (error) {
-        console.log('ERROR! Nothing is happening!');
-    }
-  }
+    // Содержание структуры
+    building: "",
+    auditorium: "",
+    discipline: "",
+    schedule: "",
+  });
 
   return (
     <div v-else-if="contacts.length">
       <div class="card mb-3" v-for="contact in contacts">
-        <div class="card-body">
-          {/* <h5 class="card-title" :style="{color: contact.marked ? 'red' : 'black'}">{{contact.name}}</h5> */}
-          {/* <h5 class="card-title">{{ discipline }}</h5> */}
-          {/* <p class="card-text">{{ auditorium }}</p> */}
-          <button class="btn btn-primary">Отметить</button>
-          <button class="btn btn-danger">Удалить</button>
+        {form.map((p) => {
+              return (
+                <div>
+                  <p>
+                    {p.building}
+                    {p.auditorium}
+                    {p.discipline}
+                    {p.schedule}
+                  </p>
+                </div>
+              );
+            })}
+          {/* <button class="btn btn-primary">Отметить</button> */}
+          {/* <button class="btn btn-danger">Удалить</button> */}
         </div>
       </div>
-    </div>
-    // <p>Контактов пока нет</p>
-  )
+  );
 };
