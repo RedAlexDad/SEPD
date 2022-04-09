51f1971af053b71f248cc6c1c8dff5e72f7fa88a
egorklesha <75128497+egorklesha@users.noreply.github.com>
2022-04-09 21:53:38 +0300

    MENU B

--- client/src/pages/Header.js
+++ client/src/pages/Header.js
@@ -33,7 +33,7 @@ export const Header = () => {
     auth.logout();
     history.push("/");
   };
-  
+
 
 
   return (
@@ -54,47 +54,55 @@ export const Header = () => {
           </h1>
         </div>
       </header> */}
-
+ 
       <center>
-        <div class="menu">
-          <ul>
-            <li>
-              <a class="menu ul li a" href>
-                <Link to="/main">Главная</Link>
-              </a>
-            </li>
-            <li>
-              <a class="menu ul li a" href>
-                <Link to="/request">Оформить заявку</Link>
-              </a>
-            </li>
-            <li>
-              <a class="menu ul li a" href>
-                <Link to="/request_tasks">Посмотреть все записи</Link>
-              </a>
-            </li>
-            <li>
-              <a class="menu ul li a" href>
-                <Link to="/contact_personal">Контакты</Link>
-              </a>
-            </li>
-            <li>
-              <a class="menu ul li a" href>
-                <Link to="/regist">Регистрация </Link>
-              </a>
-            </li>
-            <li>
-              <a class="menu ul li a" href="/" onClick={logoutHandler}>
-                Выйти
-              </a>
-            </li>
-            <li class="FIO_man_pos">
-              <p class="fw-bold">
-                {family} {name} {fatherland} {group}
-              </p>
-            </li>
-          </ul>
-        </div>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+          <div class="container-fluid">
+            <a class="navbar-brand" href="">Запись к переводчику</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav mr-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                  <a class="nav-link active" aria-current="page" href>
+                    <Link to="/main">Главная</Link>
+                  </a>
+                </li>
+                <li>
+                  <a class="nav-link active" aria-current="page" href>
+                    <Link to="/request">Оформить заявку</Link>
+                  </a>
+                </li>
+                <li>
+                  <a class="nav-link active" aria-current="page" href>
+                    <Link to="/request_tasks">Посмотреть все записи</Link>
+                  </a>
+                </li>
+                <li>
+                  <a class="nav-link active" aria-current="page" href>
+                    <Link to="/contact_personal">Контакты</Link>
+                  </a>
+                </li>
+                <li>
+                  <a class="nav-link active" aria-current="page" href>
+                    <Link to="/regist">Регистрация </Link>
+                  </a>
+                </li>
+                <li>
+                  <a class="nav-link active" aria-current="page" href="/" onClick={logoutHandler}>
+                    Выйти
+                  </a>
+                </li>
+                <li class="FIO_man_pos">
+                  <p class="fw-bold">
+                    {family} {name} {fatherland} {group}
+                  </p>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
       </center>
     </div>
   );
